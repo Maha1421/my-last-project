79775164ffde710599805db51327e79c4bd0c41c
Ma11Ha21 <m9.a11.h14.a21@gmail.com>
2023-01-06 04:19:38 +0300

    Initial Commit

---
+++ GitM/GitMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitMApp.swift
+//  GitM
+//
+//  Created by Maha Saleh Alghamdi on 13/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
